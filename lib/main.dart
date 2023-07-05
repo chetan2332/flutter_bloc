ab16c1949762cabf0af68530629b3d7209e22ff9
chetan2332 <101946181+chetan2332@users.noreply.github.com>
2023-07-06 00:32:18 +0530

    ep-1

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:connectivity/blocs/internet_bloc/internet_bloc.dart';
+import 'package:connectivity/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => InternetBloc(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: const HomeScreen(),
+        theme: ThemeData.dark(),
+      ),
+    );
+  }
+}
